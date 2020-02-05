83b68d8aefd7701ea3465bd08730e8125d9ec216
anggonzales <anggonzalesca@upt.pe>
2020-02-04 19:14:57 -0500

    Imagenes

---
+++ app/src/main/java/com/example/imagenes/MiImageLoader.java
@@ -0,0 +1,14 @@
+package com.example.imagenes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MiImageLoader extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mi_image_loader);
+    }
+}
